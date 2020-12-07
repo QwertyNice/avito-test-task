f49503fbfa0e474ea73ca0cdf0cfbddc2c02c060
QwertyNice <gaus.dmitry@yandex.ru>
2020-12-07 13:06:21 +0500

    Сhanged architecture of the Parser class

--- avito-test-task/main.py
+++ avito-test-task/main.py
@@ -8,9 +8,9 @@ import asyncio
 from secretinfo import USER, PASSWORD, HOST, DATABASE
 from db_connection import DatabaseConnector
 
+
 class Requester:
-    # FIXME
-    
+
     def __init__(self):
         self.row_url = 'https://www.avito.ru/{}'
 
@@ -18,67 +18,98 @@ class Requester:
         self.url = self.row_url.format(region)
 
     def make_request(self, params=None):
-        return requests.get(self.url, params=params)
+        return requests.get(self.url, params=params).text
 
 
 class Parser:
 
-    def __init__(self, row_answer):
+    def __init__(self, row_answer, query, skip_check=False):
+        # TODO случай skip_check = True
         self.row_answer = row_answer
+        self.error = None
+        self.__tree = self._prepare_to_parse()
+        self.valid_region = self._check_valid_region()
+        if self.valid_region:
+            if self._check_valid_query(q=query):
+                self.query = self.correct_mistake_in_query(q=query)
+        # FIXME Вернуть правильно невалидный query
+        else:
+            self.query = query
 
-    def prepare_to_parse(self):
+    def _prepare_to_parse(self):
         """Функция преобразовывает код страницы в дерево"""
-        self.tree = fromstring(self.row_answer.text)
+        return fromstring(self.row_answer)
 
-    def __check_vaild_request(self):
+    def _check_valid_region(self):
         """Функция проверяет, правильно ли пользователь ввел регион"""
-        self.__answer = self.tree.xpath('//div[@class="b-404"]/h1/text()')
-        return not self.__answer
+        answer = self.__tree.xpath('//div[@class="b-404"]/h1/text()')
+        if answer:
+            self.error = 'Введен невалидный регион'
+        return not answer
 
-    def parse(self):
+    def _check_valid_query(self, q):
+        """Функция проверяет, правильно ли пользователь ввел запрос"""
+        return self.parse_count(q=q) != 0
+
+    def parse_count(self, q):
         """Функция парсит преобразованное дерево и выводит количество обьявлений"""
-        # FIXME
-        if self.__check_vaild_request():
-            count = self.tree.xpath(
+        if q:
+            count = self.__tree.xpath(
                 '//span[starts-with(@class, "page-title-count")]/text()')
             if count:
                 count = int(count[0].replace(' ', ''))
-                return count
             else:
-                count = self.tree.xpath(
-                '//span[starts-with(@class, "category-with-counters-count")]/text()')
-                return int(count[0].replace(',', ''))
+                count = 0
+                # FIXME Исправить синтаксичесическую часть предложения
+                self.error = 'Введен невалидный запрос или количество объявлений по нему ноль'
+            return count
         else:
-            self.__answer = 'Введен неверный регион!'
-            return self.__answer
+            count = self.__tree.xpath(
+                '//span[starts-with(@class, "category-with-counters-count")]/text()')
+            count = int(count[0].replace(',', ''))
+            return count
+
+    def correct_mistake_in_query(self, q):
+        """Функция исправляет ошибки в запросе, если они есть"""
+        if q:
+            q = self.__tree.xpath('//div[starts-with(@class, "index-suggest-")]/input/@value')[0]
+        return q
 
 
 app = FastAPI()
-db_connector = DatabaseConnector()
+# db_connector = DatabaseConnector()
+
 
 @app.on_event("startup")
 async def start_consuming():
     # FIXME
     # create db_connection
     await db_connector.connect(USER, PASSWORD, HOST, DATABASE)
-    await db_connector.check_tables()
-    # asyncio.create_task(db_connector.select_db())
-    
+    asyncio.create_task(db_connector.select_db())
 
 
 @app.on_event("shutdown")
 async def shutdown():
     await db_connector.disconnect()
 
+
 @app.get("/add/{region}")
 async def read_item(region: str, query: Optional[str] = None):
-    a = Requester()
-    a.prepare_request(region=region)
-    answer = a.make_request(params={'q': query})
-    parser = Parser(answer)
-    parser.prepare_to_parse()
-    count = parser.parse()
-    return {"id": None, "count": count, "timestamp": datetime.now().timestamp()}
+    db_query = 'SELECT * FROM ...'
+    if db_query:
+        pair_id = ... # id пары под индексом ноль
+    else:
+        requester.prepare_request(region=region)
+        answer = requester.make_request(params={'q': query})
+        parser = Parser(answer)
+        parser.prepare_to_parse()
+        if not parser.check_valid_region():
+            pair_id = 'Введен невалидный регион.'
+        else:
+            query = parser.check_query(q=query)
+        # добавляем регион и запрос в SQL
+        pair_id = ... # присваиваем новому региону и запросу id
+    return {"id": pair_id}
 
 
 if __name__ == '__main__':
